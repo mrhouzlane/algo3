e3995b4c32147bc320bb37046ddf3772c009e5d0
mehdi <rhouzm@gmail.com>
2023-01-30 17:47:32 +0100

    add

--- contract.py
+++ contract.py
@@ -1,80 +1,92 @@
 from pyteal import *
-from pyteal.ast.bytes import Bytes
-from helpers import program
+from beaker import *
 
 
-def approval_program():
+class Auction(Application):
 
-    # locals
-    local_opponent = Bytes("opponent")  # byteslice
-    local_bet = Bytes("bet")  # uint64
-    local_private_play = Bytes("private_play")  # byteslice
-    local_play = Bytes("play")  # byteslice
+    # Global state 
 
-    op_challenge = Bytes("challenge")
-    op_accept = Bytes("accept")
-    op_reveal = Bytes("reveal")
-
-    # Subroutines 
     
-    @Subroutine(TealType.none)
-    def reset(account: Expr):
+    # 2 Global Bytes 
+    # owner : address 
+    owner = ApplicationStateValue(stack_type=TealType.bytes)
+    # highest_bidder : address
+    highest_bidder = ApplicationStateValue(stack_type=TealType.bytes)
+
+    # 2 Global Ints 
+    # highest_bid : uint64
+    highest_bid = ApplicationStateValue(stack_type=TealType.uint64)
+    # bid_end : uint64 
+    auction_end = ApplicationStateValue(stack_type=TealType.uint64)
+
+
+    # 1. Create the auction contract
+     
+    @create # this decorator tells Beaker this function .. 
+    def create(self):
         return Seq(
-            App.localPut(account, local_opponent, Bytes("")),
-            App.localPut(account, local_bet, Int(0)),
-            App.localPut(account, local_private_play, Bytes("")),
-            App.localPut(account, local_play, Bytes("")),
+            self.owner.set(Txn.sender()), # Person who initialize & sender() returns the Pyteal expression 
+            self.highest_bidder.set(Bytes("")), 
+            self.highest_bid.set(Int(0)),
+            self.bid_end.set(Int(0))
         )
 
 
-    @Subroutine(TealType.none)
-    def create_challenge():
-        return Reject()
+    # 2. Start_auction 
 
-    @Subroutine(TealType.none)
-    def accept_challenge():
-        return Reject()
+    #  * Length : duration of the auction *
+    @external 
+    def start_auction(self, payment: abi.PaymentTransaction, length : abi.Uint64, start_price: abi.Uint64):  
+        # fund the contract with 0.1 ALGO = 0.1 * 10^6 microalgos = 100 000 microalgos
+        # set the bid_end to a certain time
+        # fix a bid start amount 
+        payment = payment.get()
+        return Seq(
+            Assert(payment.receiver() == Global.current_application_address()),  # Checking that payment is made to this contract 
+            Assert(payment.amount() == Int(100_000)),
+            # set the bid_end 
+            self.bid_end.set(Global.latest_timestamp() + length.get() ),
+            self.highest_bid.set(start_price.get())
+        )
+    
+    # 3. Handle incoming bids 
 
+    @internal(TealType.none)
+    # Pay back to user if not highest bid 
 
-    @Subroutine(TealType.none)
-    def reveal():
-        return Reject()
-
-    return program.event(
-        init=Approve(),
-        opt_in=Seq(
-            reset(Int(0)),
-            Approve(),
-        ),
-        no_op=Seq(
-            Cond(
-                [
-                    Txn.application_args[0] == op_challenge,
-                    create_challenge(),
-                ],
-                [
-                    Txn.application_args[0] == op_accept,
-                    accept_challenge(),
-                ],
-                [
-                    Txn.application_args[0] == op_reveal,
-                    reveal(),
-                ],
+    def pay(self, receiver: Expr, amount: Expr):
+        return Seq(
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields(
+                {
+                    TxnField.type_enum: TxnType.Payment,
+                    TxnField.receiver: receiver, 
+                    TxnField.amount : amount,
+                    TxnField.fee : Int(0),
+                }
             ),
-            Reject(),
-        ),
-    )
+            InnerTxnBuilder.Submit(),
+        )
 
 
-def clear_state_program():
-    return Int(0)
+    @external 
+    def bidding(self, payment: abi.PaymentTransaction):
+        payment = payment.get()
+
+        return Seq(
+            Assert(Global.latest_timestamp() < self.auction_end.get()),
+            # Verifying payment transaction 
+            Assert(payment.amount() > self.highest_bid.get()),
+            Assert(Txn.sender() == payment.sender()),
+            # Return previous bid 
+            If(self.highest_bidder.get() != Bytes("") , self.pay(self.highest_bidder.get(), self.highest_bid.get())),
+            # Set global state
+            self.highest_bidder.set(payment.sender()),
+            self.highest_bid.set(payment.amount()),
+        )
 
 
-if __name__ == "__main__":
-    with open("vote_approval.teal", "w") as f:
-        compiled = compileTeal(approval_program(), Mode.Application, version=6)
-        f.write(compiled)
+    # 3. End_auction 
 
-    with open("vote_clear_state.teal", "w") as f:
-        compiled = compileTeal(clear_state_program(), Mode.Application, version=6)
-        f.write(compiled)
+
+
